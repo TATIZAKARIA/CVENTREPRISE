dd372fad353ef9db3747a226e6489d7adf951fa7
TATIZAKARIA <tati_zakaria@yahoo.fr>
2021-07-07 22:49:01 +0200

    Premiére partie

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-// TYPED
+//NAVBAR
+let lastScrolltop=0;
+navbar= document.querySelector('.navbar');
+window.addEventListener('scroll',function(){
+const scrollTop = window.pageTOffset ||
+this.document.documentElement.scrollTop;
+if (scrrollTop>lastscrollTop) {
+    navbar.style.top="-50px";
+}else{
+    navbar.style.top=0;
+}
+lastScrolltop=scrollTop;
+});
 
+
+// TYPED
 var typed = new Typed('.typed', {
     strings: ["Je suis une personne dynamique,ponctuelle et trés rigoureuse,sportive dans l'âme ce qui me permet de travailler en équipe et de répondre aux demandes de l'entreprise, dans le but de satisfaire le client.,Toutes ces qualités me permettent d'avoir le sens du détail et me rendent trés ouvert car J'adore les challenges afin de décrocher des résultats pour chaque objectif ciblé, en respectant l'environement du travail.,Il suffit juste d'un petit click pour en savoir plus sur moi...!"],
     typeSpeed: 40,
@@ -8,9 +22,5 @@ var typed = new Typed('.typed', {
     // loop: false
 
 });
-//     Je suis une personne dynamique,ponctuelle et trés rigoureuse , sportive dans l'âme ce qui me permet de travailler en équipe et de répondre aux demandes de l'entreprise, dans le but de satisfaire le client.
 
-// Toutes ces qualités me permettent d'avoir le sens du détail et me rendent trés ouvert car J'adore les challenges afin de décrocher des résultats pour chaque objectif ciblé, en respectant l'environement du travail.
-
-// Il suffit juste d'un petit click pour en savoir plus sur moi...!
  
