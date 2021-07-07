8113e0b67dd5e6c183ac21918924509b9a39efca
TATIZAKARIA <tati_zakaria@yahoo.fr>
2021-07-07 21:32:06 +0200

    ESSAI PARTIE1

---
+++ script.js
@@ -0,0 +1,16 @@
+// TYPED
+
+var typed = new Typed('.typed', {
+    strings: ["Je suis une personne dynamique,ponctuelle et trés rigoureuse,sportive dans l'âme ce qui me permet de travailler en équipe et de répondre aux demandes de l'entreprise, dans le but de satisfaire le client.,Toutes ces qualités me permettent d'avoir le sens du détail et me rendent trés ouvert car J'adore les challenges afin de décrocher des résultats pour chaque objectif ciblé, en respectant l'environement du travail.,Il suffit juste d'un petit click pour en savoir plus sur moi...!"],
+    typeSpeed: 40,
+    // backSpeed: 0,
+    // smartBackspace: true, // this is a default
+    // loop: false
+
+});
+//     Je suis une personne dynamique,ponctuelle et trés rigoureuse , sportive dans l'âme ce qui me permet de travailler en équipe et de répondre aux demandes de l'entreprise, dans le but de satisfaire le client.
+
+// Toutes ces qualités me permettent d'avoir le sens du détail et me rendent trés ouvert car J'adore les challenges afin de décrocher des résultats pour chaque objectif ciblé, en respectant l'environement du travail.
+
+// Il suffit juste d'un petit click pour en savoir plus sur moi...!
+ 
